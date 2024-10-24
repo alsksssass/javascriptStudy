f777042f556304d7b23de5384a569d2ba2e36570
smj53 <smjeong53@gmail.com>
2024-10-24 12:30:08 +0900

    fix

--- chapter_20/semjeong/1_code.js
+++ chapter_20/semjeong/1_code.js
@@ -1,6 +1,6 @@
 function example() {
   x = 10; // 암묵적 전역을 만들지만 참조하지는 않아 에러 발생 x
-  delete Object.prototype;
+  delete Object.prototype; // TypeError: Cannot delete property 'prototype' of function Object()
   function inner() {}
   // 함수 삭제 -> SyntaxError
   delete inner;
@@ -9,6 +9,5 @@ function example() {
 function corrected() {
   'use strict';
   x = 10;
-  delete Object.prototype;
   function inner() {}
 }
