e29334cb0e2e69daa29e6d07ad1991c12b1b29e0
변상훈 <byeonsanghun@byun.local>
2024-10-30 17:01:55 +0900

    sabyun add

--- chapter_25/sabyun/isMatch.js
+++ chapter_25/sabyun/isMatch.js
@@ -1,15 +1,12 @@
 class Line {
     constructor(arr) {
-        this.x1 = arr[0][0];
-        this.y1 = arr[0][1];
-        this.x2 = arr[1][0];
-        this.y2 = arr[1][1];
+        [this.x1 , this.y1] = arr[0];
+        [this.x2 , this.y2] = arr[1];
     }
     // 두 선분이 평행한지 체크하는 메서드 추가
     isParallel(otherLine) {
-        const crossProduct = (this.x2 - this.x1) * (otherLine.y2 - otherLine.y1) - 
+        return (this.x2 - this.x1) * (otherLine.y2 - otherLine.y1) ===
                              (this.y2 - this.y1) * (otherLine.x2 - otherLine.x1);
-        return crossProduct === 0;
     }
 };
 
@@ -20,5 +17,4 @@ function solution(dots) {
 }
 
 
-
-//5번자꾸 틀림
+//5번 틀림 ㅠ
