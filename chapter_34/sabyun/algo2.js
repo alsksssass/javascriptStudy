f5de526c16b0eba5ae624db51f4466b5967fe80a
변상훈 <byeonsanghun@byun.local>
2024-11-11 12:44:39 +0900

    sabyun add

--- chapter_34/sabyun/algo2.js
+++ chapter_34/sabyun/algo2.js
@@ -1,17 +1,25 @@
-// function solution(numbers) {
-// 	let answer = [];
-// 	for(let i =0; i < numbers.length; i++)
-// 		for(let j = 0; j < numbers.length; j++){
-// 			if(j ===i) continue;
-// 			answer.push(numbers[i] + numbers[j]);
-// 	}
-//     return [...new Set(answer.sort((a,b)=>a-b))];
-// }
 function solution(numbers) {
+	let answer = [];
+	for(let i =0; i < numbers.length; i++)
+		for(let j = 0; j < numbers.length; j++){
+			if(j ===i) continue;
+			answer.push(numbers[i] + numbers[j]);
+	}
+    return [...new Set(answer.sort((a,b)=>a-b))];
+}
+function solution1(numbers) {
 	let answer = [];
 	numbers.forEach ((v,i)=>{numbers.forEach((a,b)=> {i!==b ? answer.push(a+v) : null});});
 	return[...new Set(answer.sort())];
 }
 
-
-console.log(solution([2,1,3,4,1]));
+const temp = [];
+for(let i =0; i < 100; i++)
+	temp.push(Math.floor(Math.random()*100 +1));
+console.log(temp);
+console.time('double');
+console.log(solution(temp));
+console.timeEnd('double');
+console.time('function');
+console.log(solution1(temp));
+console.timeEnd('function');
