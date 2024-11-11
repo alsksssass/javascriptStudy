9364540a5112ff494d6803486092123a69c13c7e
변상훈 <byeonsanghun@byun.local>
2024-11-11 13:08:12 +0900

    sabyun add

--- chapter_34/sabyun/algo.js
+++ chapter_34/sabyun/algo.js
@@ -4,8 +4,13 @@ function solution(l, r) {
 		if((String(i).replace(/[05]/g,'')).length === 0)
 			answer.push(i);
 	}
-
-    return answer.length === 0 ? answer : [-1];
+    return answer.length !== 0 ? answer : [-1];
 }
-
+const solution1 = (l, r) => (a = Array.from({length: r-l+1}, (_, i) => l+i).filter(n => String(n).split('').every(d => d === '0' || d === '5'))).length ? a : [-1];
+console.time('me');
 console.log(solution(5,555));
+console.timeEnd('me');
+console.time('one');
+console.log(solution1(5,555));
+console.timeEnd('one');
+
