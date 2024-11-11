86a0959c9bc01dfbde7f0b1aee61736201091b76
변상훈 <byeonsanghun@byun.local>
2024-11-11 12:27:03 +0900

    sabyun add

--- chapter_34/sabyun/algo2.js
+++ chapter_34/sabyun/algo2.js
@@ -1,11 +1,17 @@
+// function solution(numbers) {
+// 	let answer = [];
+// 	for(let i =0; i < numbers.length; i++)
+// 		for(let j = 0; j < numbers.length; j++){
+// 			if(j ===i) continue;
+// 			answer.push(numbers[i] + numbers[j]);
+// 	}
+//     return [...new Set(answer.sort((a,b)=>a-b))];
+// }
 function solution(numbers) {
 	let answer = [];
-	for(let i =0; i < numbers.length; i++)
-		for(let j = 0; j < numbers.length; j++){
-			if(j ===i) continue;
-			answer.push(numbers[i] + numbers[j]);
-	}
-    return [...new Set(answer.sort((a,b)=>a-b))];
+	numbers.forEach ((v,i)=>{numbers.forEach((a,b)=> {i!==b ? answer.push(a+v) : null});});
+	return[...new Set(answer.sort())];
 }
+
 
 console.log(solution([2,1,3,4,1]));
