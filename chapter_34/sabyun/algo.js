9f54273b3b905fc078a7b815c6324031f227aaf0
변상훈 <byeonsanghun@byun.local>
2024-11-12 14:48:22 +0900

    sabyun add

--- chapter_34/sabyun/algo.js
+++ chapter_34/sabyun/algo.js
@@ -7,6 +7,7 @@ function solution(l, r) {
     return answer.length !== 0 ? answer : [-1];
 }
 const solution1 = (l, r) => (a = Array.from({length: r-l+1}, (_, i) => l+i).filter(n => String(n).split('').every(d => d === '0' || d === '5'))).length ? a : [-1];
+
 console.time('me');
 console.log(solution(5,555));
 console.timeEnd('me');
